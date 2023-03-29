e570b2b421957945cca829d131015b09d6c96eac
linshouan <lsa13322289229@163.com>
2023-03-29 17:05:25 +0800

    no message

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/account/management/AccountManagementController.java
@@ -0,0 +1,12 @@
+package com.ruoyi.web.controller.account.management;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accountManagement")
+@Slf4j
+public class AccountManagementController {
+//    public
+}
--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">后台管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -56,7 +56,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>
+<!--      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>-->
     </div>
   </div>
 </template>
@@ -91,7 +91,7 @@ export default {
       // 验证码开关
       captchaEnabled: true,
       // 注册开关
-      register: false,
+      register: true,
       redirect: undefined
     };
   },
@@ -106,6 +106,7 @@ export default {
   created() {
     this.getCode();
     this.getCookie();
+    fairyDustCursor();
   },
   methods: {
     getCode() {
@@ -153,16 +154,136 @@ export default {
     }
   }
 };
+
+
+function fairyDustCursor() {
+  var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var cursor = {
+    x: width / 2,
+    y: width / 2
+  };
+  var particles = [];
+
+  function init() {
+    bindEvents();
+    loop()
+  }
+
+  function bindEvents() {
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchstart', onTouchMove);
+    window.addEventListener('resize', onWindowResize)
+  }
+
+  function onWindowResize(e) {
+    width = window.innerWidth;
+    height = window.innerHeight
+  }
+
+  function onTouchMove(e) {
+    if (e.touches.length > 0) {
+      for (var i = 0; i < e.touches.length; i++) {
+        addParticle(e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random() * possibleColors.length)])
+      }
+    }
+  }
+
+  function onMouseMove(e) {
+    cursor.x = e.clientX;
+    cursor.y = e.clientY;
+    addParticle(cursor.x, cursor.y, possibleColors[Math.floor(Math.random() * possibleColors.length)])
+  }
+
+  function addParticle(x, y, color) {
+    var particle = new Particle();
+    particle.init(x, y, color);
+    particles.push(particle)
+  }
+
+  function updateParticles() {
+    for (var i = 0; i < particles.length; i++) {
+      particles[i].update()
+    }
+    for (var i = particles.length - 1; i >= 0; i--) {
+      if (particles[i].lifeSpan < 0) {
+        particles[i].die();
+        particles.splice(i, 1)
+      }
+    }
+  }
+
+  function loop() {
+    requestAnimationFrame(loop);
+    updateParticles()
+  }
+
+  function Particle() {
+    this.character = "*";
+    this.lifeSpan = 200;
+    this.initialStyles = {
+      "position": "fixed",
+      "top": "0",
+      "display": "block",
+      "pointerEvents": "none",
+      "z-index": "99999999",
+      "fontSize": "16px",
+      "will-change": "transform"
+    };
+    this.init = function (x, y, color) {
+      this.velocity = {
+        x: (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+        y: 1
+      };
+      this.position = {
+        x: x - 10,
+        y: y - 20
+      };
+      this.initialStyles.color = color;
+      this.element = document.createElement('span');
+      this.element.innerHTML = this.character;
+      applyProperties(this.element, this.initialStyles);
+      this.update();
+      document.body.appendChild(this.element)
+    };
+    this.update = function () {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      this.lifeSpan--;
+      this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")"
+    }
+    this.die = function () {
+      this.element.parentNode.removeChild(this.element)
+    }
+  }
+
+  function applyProperties(target, properties) {
+    for (var key in properties) {
+      target.style[key] = properties[key]
+    }
+  }
+  init()
+}
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
 .login {
   display: flex;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
+  width: auto;
+  //background-image: url("../assets/images/login-background.jpg");
+  background-image: url("./3.jpg");
   background-size: cover;
+  background-repeat: no-repeat;
+  //保证盒子始终在窗口中心
+  background-position: center center;
+  //避免窗口周围留白
+  width: 100%;
+  height: 100%;
 }
 .title {
   margin: 0px auto 30px auto;
@@ -174,6 +295,7 @@ export default {
   border-radius: 6px;
   background: #ffffff;
   width: 400px;
+  margin-left: 15%;
   padding: 25px 25px 5px 25px;
   .el-input {
     height: 38px;
